9cee89f9129d938c7718b6a9289f97b0306b4b32
HQ106347 <jiri@solar.dk>
2022-02-03 13:02:59 +0100

    Added sample repository

---
+++ example/main.cpp
@@ -0,0 +1,6 @@
+#include "MyLib/library.h"
+
+int main() {
+    hello();
+    return 0;
+}
---
+++ source/MyLib/library.cpp
@@ -0,0 +1,7 @@
+#include "../../include/MyLib/library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
